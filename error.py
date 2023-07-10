6b74265e32a885c7cbda1421c8d75231c05a0d5e
Ben Borges <info@buzzworkers.com>
2023-07-10 21:28:20 +0200

    Update error.py
    fix

--- error.py
+++ error.py
@@ -1,23 +1,35 @@
-from dotenv import load_dotenv
+import time
 import os
 import re
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from dotenv import load_dotenv
+
+load_dotenv()  # load environment variables from .env file
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-        with open(os.getenv('LOG_PATH'), 'r') as file:
-            lines = file.readlines()
-            for line in lines[-2:]:
-                timestamp = re.search(r'(\w{3} \d{1,2} \d{2}:\d{2}:\d{2})', line)
-                if 'HTTP/1.1" 400' in line or 'HTTP/1.1" 404' in line or 'Unknown error' in line:
-                    host = re.search(r'host: "([^"]+)', line)
-                    if timestamp is not None:
-                        self.trigger_webhook('nginx error', timestamp.group(), host.group(1) if host else 'Unknown')
-                elif 'unresponsive' in line and not '0 unresponsive' in line:
-                    if timestamp is not None:
-                        self.trigger_webhook('App became unresponsive', timestamp.group(), 'box.log')
+        print(f'Event type: {event.event_type}  path : {event.src_path}')
+        log_paths = os.getenv('LOG_PATHS').split(',')
+        if event.src_path in log_paths:
+            with open(event.src_path, "r") as file:
+                last_two_lines = file.readlines()[-2:]
+                for line in last_two_lines:
+                    timestamp = re.search(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', line)
+                    host = re.search(r'host: "([^"]*)"', line)
+                    if "HTTP/1.1\" 404" in line: 
+                        print("Error 404 found!")
+                        if timestamp:
+                            self.trigger_webhook('HTTP 404 error occurred in nginx', timestamp.group(), host.group(1) if host else 'Unknown')
+                    elif "Unknown error" in line:
+                        print("Unknown error found!")
+                        if timestamp:
+                            self.trigger_webhook('Unknown error occurred in nginx', timestamp.group(), host.group(1) if host else 'Unknown')
+                    elif 'unresponsive' in line and not '0 unresponsive' in line:
+                        print("Unresponsive app found!")
+                        if timestamp:
+                            self.trigger_webhook('App became unresponsive', timestamp.group(), host.group(1) if host else 'Unknown')
 
     def trigger_webhook(self, error_msg, timestamp, host):
         url = os.getenv('WEBHOOK_URL')
@@ -26,11 +38,13 @@ class MyHandler(FileSystemEventHandler):
         print("Webhook triggered, status code:", response.status_code)
 
 if __name__ == "__main__":
-    load_dotenv()
     event_handler = MyHandler()
     observer = Observer()
-    observer.schedule(event_handler, path=os.getenv('LOG_PATH'), recursive=False)
+    log_paths = os.getenv('LOG_PATHS').split(',')
+    for log_path in log_paths:
+        observer.schedule(event_handler, path=log_path, recursive=False)
     observer.start()
+
     try:
         while True:
             time.sleep(1)
