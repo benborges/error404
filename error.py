09e726be3783536059d259bf3d23c937e1850643
Ben Borges <info@buzzworkers.com>
2023-07-10 17:31:14 +0200

    Update error.py

--- error.py
+++ error.py
@@ -3,23 +3,27 @@ import os
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from dotenv import load_dotenv
+
+load_dotenv()  # load environment variables from .env file
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
         print(f'Event type: {event.event_type}  path : {event.src_path}')
         if event.src_path == "/var/log/nginx/error.log":
             with open(event.src_path, "r") as file:
-                last_line = file.readlines()[-2]
-                if "HTTP/1.1\" 404" in last_line: # check if the last line of the file contains 'HTTP/1.1" 404'
-                    print("Error 404 found!")
-                    self.trigger_webhook('HTTP 404 error occurred in nginx')
-                elif "Unknown error" in last_line:
-                    print("Unknown error found!")
-                    self.trigger_webhook('Unknown error occurred in nginx')
+                last_two_lines = file.readlines()[-2:]
+                for line in last_two_lines:
+                    if "HTTP/1.1\" 404" in line: 
+                        print("Error 404 found!")
+                        self.trigger_webhook('HTTP 404 error occurred in nginx')
+                    elif "Unknown error" in line:
+                        print("Unknown error found!")
+                        self.trigger_webhook('Unknown error occurred in nginx')
 
-    def trigger_webhook(self):
-        url = 'https://your-webhook-url'
-        data = {'text': 'HTTP 404 error occurred in nginx'}
+    def trigger_webhook(self, error_msg):
+        url = os.getenv('WEBHOOK_URL')
+        data = {'text': error_msg}
         response = requests.post(url, json=data)
         print("Webhook triggered, status code:", response.status_code)
 
